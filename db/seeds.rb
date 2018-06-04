c9c7c2f5b907161343b263ed3c3d63ad6b545b57
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-04 16:38:06 +0900

    Initial setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "oladhari", email: "oussama@gmail.com", password:  "topsecret", password_confirmation: "topsecret", address: "Nara, Japan")
+puts
+puts "User Added completed"
+
+User.create(username: "reymon29", email: "rey@gmail.com", password:  "topsecret", password_confirmation: "topsecret", address: "Tokyo, Japan")
+puts
+puts "User Added completed"
+
+User.create(username: "hcquach", email: "hc@gmail.com", password:  "topsecret", password_confirmation: "topsecret", address: "Paris, France")
+puts
+puts "User Added completed"
+
+User.create(username: "gianluca24", email: "luca@gmail.com", password:  "topsecret", password_confirmation: "topsecret", address: "Manila, Philippines")
+puts
+puts "User Added completed"
