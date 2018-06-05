fced11f18731352e11c40c00c0e33742dacd982a
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-05 13:33:16 +0900

    Correction of the missing controllers

---
+++ app/controllers/collaborations_controller.rb
@@ -0,0 +1,25 @@
+class CollaborationsController < ApplicationController
+  before_action :set_collaboration, only: [:new, :destroy]
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+    if @collaboration.user == current_user
+      @collaboration.destroy
+    end
+  end
+
+  private
+
+  def set_collaboration
+    @collaboration = Collaboration.find(params[:id])
+  end
+
+  def collaboration_params
+    params.require(:collaboration).permit(:status, :match_id)
+  end
+end
---
+++ app/controllers/gratitudes_controller.rb
@@ -0,0 +1,53 @@
+class GratitudesController < ApplicationController
+  before_action :set_gratitude, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @gratitudes = Gratitude.where(user: current_user)
+  end
+
+  def new
+    @gratitude = Gratitude.new
+  end
+
+  def create
+    @gratitude = Gratitude.new(gratitude_params)
+    if @gratitude.save
+      redirect_to gratitude_path(@gratitude)
+    else
+      flash[:alert] = "Please try again"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @gratitude.update(gratitude_params)
+      redirect_to gratitude_path(@gratitude)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @gratitude.user == current_user
+      @gratitude.destroy
+      redirect_to gratitudes_path
+    end
+  end
+
+  private
+
+  def set_gratitude
+    @gratitude = Gratitude.find(params[:id])
+  end
+
+  def gratitude_params
+    params.require(:gratitude).permit(:title, :photo, :description, :category_id, :user_id, :collaboration_id)
+  end
+end
