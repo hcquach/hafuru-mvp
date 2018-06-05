190ec9914740fcce5100e3b110a8e813c148af4e
oussama ladhari <oussama.ladhari@gmail.com>
2018-06-05 20:03:48 +0900

    working on matching logic

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -13,6 +13,43 @@ class MatchesController < ApplicationController
   end
 
   def create
+
+    @match = Match.new(match_params)
+    # @gratitude = matching_gratitude
+    @gratitude = @match.matching_gratitude
+    # matching_gratitude.user = current_user
+    # @match.matching_gratitude = Gratitude.find(params[:matching_gratitude_id])
+    # matching_gratitude_arry = matching_gratitude.split(/\W+/)
+    #how to find the matched_gratitude
+    # gratitudes_to_match = Gratitude.where.not(user: current_user)
+    gratitudes_to_match = Gratitude.where("user != ? AND collaboration_id is ?", current_user, nil)
+    matching_category = matching_gratitude.category_id
+    gratitudes_to_match.each do |gratitude|
+     if gratitude.category_id == matching_category
+
+
+
+
+
+      # gratitude.title.split(/\W+/).each do |word|
+      #   counter = {}
+      #   word_count = 0
+      #   matching_gratitude_arry.each do |matching|
+      #     if matching.downcase == word.downcase
+      #       word_count += 1
+      #       counter[word] = word_count
+      #     else
+      #       word.count =0
+      #     end
+      #   else
+      #   @match.matched_gratitude = 0
+      # end
+
+
+
+
+
+    @match.matched_gratitude = Gratitude.find(params[:matched_gratitude_id])
   end
 
   def destroy
