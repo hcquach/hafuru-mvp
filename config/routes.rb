4941de4ec5cd3d1ab98b73448902a2f167b6e7bd
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-05 14:28:55 +0900

    Just template

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :gratitudes  do
+    resources :matches, only: [ :index, :show, :create, :destroy ]
+  end
+  resources :collaborations, only: [ :create, :destroy ] do
+    resources :collaboration_items
+  end
+
+  get 'credits', to: 'pages#credits', as: :credits
+  get 'shop', to: 'pages#shop', as: :shop
 end
