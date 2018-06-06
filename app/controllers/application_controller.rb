f246984ac74d1793c00ce728daf25b84022dafe2
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-06 13:43:18 +0900

    With some corrections on gratitude index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
+  def after_sign_in_path_for(resource)
+    gratitudes_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   private
 
   def skip_pundit?
