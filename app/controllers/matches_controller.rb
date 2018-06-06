875bbda6f46fd74193bbc4a4e13662afb8f0ccc0
oussama ladhari <oussama.ladhari@gmail.com>
2018-06-06 11:30:52 +0900

    testing match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -13,24 +13,41 @@ class MatchesController < ApplicationController
   end
 
   def create
-
     @match = Match.new(match_params)
-    # @gratitude = matching_gratitude
-    @gratitude = @match.matching_gratitude
-    # matching_gratitude.user = current_user
-    # @match.matching_gratitude = Gratitude.find(params[:matching_gratitude_id])
-    # matching_gratitude_arry = matching_gratitude.split(/\W+/)
-    #how to find the matched_gratitude
-    # gratitudes_to_match = Gratitude.where.not(user: current_user)
-    gratitudes_to_match = Gratitude.where("user != ? AND collaboration_id is ?", current_user, nil)
-    matching_category = matching_gratitude.category_id
-    gratitudes_to_match.each do |gratitude|
-     if gratitude.category_id == matching_category
+    @gratitude = Gratitude.find(params[:matching_gratitude_id])
+    # @gratitude = @match.matching_gratitude
+    # gratitudes_to_match = Gratitude.where("user != ? AND collaboration_id is ? AND match_status is ?", current_user, nil, false)
+    gratitudes_to_match = Gratitude.joins(:user).where("users.id != ? AND collaboration_id is ? AND match_status = ?", current_user.id, nil, false)
+    # matched_gratitudes = []
+    # gratitudes_to_match.each do |gratitude|
+    #  if gratitude.category_id == @gratitude.category_id
+    #   matched_gratitudes << gratitude
+    # end
+    matched_gratitude = gratitudes_to_match.find do |gratitude|
+      gratitude.category_id == @gratitude.category_id
+    end
+    # matched_gratitude = matched_gratitudes.take(1)
+    @match.matching_gratitude = @gratitude
+    @match.matched_gratitude = matched_gratitude
+    if @match.save
+      # matched_gratitude.is_matched!
+      matched_gratitude.match_status = true
+      matched_gratitude.save
+      @gratitude.match_status = true
+      @gratitude.save
+      redirect_to gratitude_match_path(@match)
+
+    else
+      flash[:notice] "No Match, try another Gratitude"
+      redirect_to gratitudes_path
+    end
 
 
-
-
-
+      # matching_gratitude.user = current_user
+      # @match.matching_gratitude = Gratitude.find(params[:matching_gratitude_id])
+      # matching_gratitude_arry = matching_gratitude.split(/\W+/)
+      #how to find the matched_gratitude
+      # gratitudes_to_match = Gratitude.where.not(user: current_user)
       # gratitude.title.split(/\W+/).each do |word|
       #   counter = {}
       #   word_count = 0
@@ -44,12 +61,6 @@ class MatchesController < ApplicationController
       #   else
       #   @match.matched_gratitude = 0
       # end
-
-
-
-
-
-    @match.matched_gratitude = Gratitude.find(params[:matched_gratitude_id])
   end
 
   def destroy
@@ -66,6 +77,6 @@ class MatchesController < ApplicationController
   end
 
   def match_params
-    params.require(:macth).permit(:matched_gratitude_id, :matching_gratitude_id)
+    params.require(:match).permit(:matched_gratitude_id, :matching_gratitude_id)
   end
 end
