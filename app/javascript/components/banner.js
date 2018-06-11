b8f0fe2d184bad7a0f30dce5822dfdc1c4527979
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-11 16:19:09 +0900

    Correcting some JS errors

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
-  new Typed('#banner-typed-text', {
+  if (document.getElementById('banner-typed-text')) { // protect other pages
+    new Typed('#banner-typed-text', {
     strings: ["Express gratitude, be happy.", "Match on this amazing frequency."],
     typeSpeed: 50,
     loop: true
-  });
+    });
+  }
 }
 
 export { loadDynamicBannerText };
