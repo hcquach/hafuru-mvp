2a8bfa6f39d14b58d8a11e8d5f1d443536548d36
oussama ladhari <oussama.ladhari@gmail.com>
2018-06-12 19:51:01 +0900

    collaboration status is working perfectly now

--- app/models/collaboration_item.rb
+++ app/models/collaboration_item.rb
@@ -4,18 +4,18 @@ class CollaborationItem < ApplicationRecord
   belongs_to :matched_user_gratitude, class_name: :Gratitude, foreign_key: :matched_user_gratitude_id, optional: true
 
 
-  after_create :update_collaboration_status
+  before_create :update_collaboration_status
 
   def update_collaboration_status
-    if CollaborationItem.where(collaboration_id: collaboration.id).count >= 0
+    if collaboration.collaboration_items.count == 30
       collaboration.status = 0
-    elsif CollaborationItem.where(collaboration_id: collaboration.id).count >= 5
+    elsif collaboration.collaboration_items.count >= 20
       collaboration.status = 1
-    elsif CollaborationItem.where(collaboration_id: collaboration.id).count >= 10
+    elsif collaboration.collaboration_items.count >= 10
       collaboration.status = 2
-    elsif CollaborationItem.where(collaboration_id: collaboration.id).count >= 20
+    elsif collaboration.collaboration_items.count >= 5
       collaboration.status = 3
-    elsif CollaborationItem.where(collaboration_id: collaboration.id).count = 30
+    elsif collaboration.collaboration_items.count >= 0
       collaboration.status = 4
     end
     collaboration.save
