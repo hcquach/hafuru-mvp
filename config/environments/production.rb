c751af4fcc8b0f3fcb73f10d04723106f3dc790b
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-13 14:40:22 +0900

    Changed gemfile and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'ws://hafuru.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://hafuru.com' ]
+  config.action_cable.allowed_request_origins = [ 'http://www.hafuru.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
